6681b3a9ac36da39d3eb78ef4a6bf3ab52c74fa0
Moschifire <markoladeinde@gmail.com>
2023-02-16 00:31:23 -0800

    comments edited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 /**
- * funtion main - this is a description
-*/
+ * main - this is a description
+ */
 int main(void)
 {
 	char myStr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(myStr);
+
+/**
+ * Return: return 0
+ */
 	return (0);
 }
