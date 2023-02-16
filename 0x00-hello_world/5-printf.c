fec5a216a22121a7fdfdddf9b8e037996fc1baed
Moschifire <markoladeinde@gmail.com>
2023-02-16 00:45:52 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main -this is a desriptuon
+ * Return: return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
