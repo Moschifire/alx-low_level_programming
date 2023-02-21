5f5f6984bb7483de27984bbf4c9f8e902152ac01
Moschifire <markoladeinde@gmail.com>
2023-02-21 04:04:42 -0800

    task 0 edited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,19 @@
 #include "main.h"
 /**
- * _putchar - writes the character to main.h
- * Return: 1 on success and -1 on error
+ * main - prints _putchar followed by a new line
+ * Return: Always 0
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	rerurn (0);
 }
