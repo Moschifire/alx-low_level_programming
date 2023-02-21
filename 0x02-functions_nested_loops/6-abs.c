26be9f73d335a98dc3b138cb7f333220241f87a5
Moschifire <markoladeinde@gmail.com>
2023-02-21 04:32:12 -0800

    6 edited

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
  * _abs - prints the absolute value of an integer
+ * @i: i is tge integer to be converted
  * Return: absolute value of the input
  */
-int _abs(int)
+int _abs(int i)
 {
 	if (i < 0)
 		i = i * -1;
