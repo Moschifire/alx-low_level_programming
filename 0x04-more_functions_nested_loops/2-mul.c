176afa8dadbb7f921263fe0238e4fe514b128d47
Moschifire <markoladeinde@gmail.com>
2023-02-22 23:04:26 -0800

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two characters
+ * @a: first chatacter to be multiplied
+ * @b: second character to be multiplied
+ * Return: the result of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
