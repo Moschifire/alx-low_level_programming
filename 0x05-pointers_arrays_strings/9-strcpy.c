b95a49bc0e185366fee2ba13c3c39e00acc89906
Moschifire <markoladeinde@gmail.com>
2023-02-27 00:05:51 -0800

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcpy - copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest
+ * _strcpy - copies the string pointed to by src
  * @dest: a buffer to copy the string to
  * @src: the source string
  */
