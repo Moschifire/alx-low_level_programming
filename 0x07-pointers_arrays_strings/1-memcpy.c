df1c210683ad03d7b22ce4384eadba69f5383408
Moschifire <markoladeinde@gmail.com>
2023-03-06 03:16:48 -0800

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,7 +7,7 @@
  * @n: The number of bytes to copy from @src
  * Return: A pointer to the destination buffer @dest
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+void *_memcpy(void *dest, void *src, size_t n)
 {
 	unsigned int index;
 	unsigned char *destination = dest;
